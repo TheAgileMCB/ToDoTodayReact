a5c15fc0962131eb11e39fa34aecbc468939df21
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-22 17:33:11 -0500

    built about us page

---
+++ todo-today-front-end/src/components/about/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default About = () => {
+    return (
+        <div className="text-body">
+            <h1>About Us</h1>
+            <p>
+                <span>We're invested in seeing you get it To-DONE!&reg;</span>
+                <br /><br />
+                Distillery ramps jean shorts intelligentsia portland, typewriter direct trade blog VHS. Ugh copper mug keffiyeh raclette trust fund art party cronut tote bag tacos woke PBR&B. Celiac hoodie shaman before they sold out pok pok glossier gentrify. Squid organic church-key, XOXO tumblr unicorn pok pok gluten-free air plant vice portland.
+                <br /><br />
+                Seitan next level chillwave stumptown, gluten-free normcore salvia vexillologist pour-over pop-up mumblecore. Vaporware letterpress air plant, direct trade affogato beard paleo you probably haven't heard of them copper mug taxidermy keytar mustache skateboard. Blue bottle shaman venmo DIY. Art party you probably haven't heard of them jean shorts cred, sriracha chartreuse succulents bespoke truffaut prism tote bag. Chambray deep v neutra bushwick, migas thundercats kombucha pitchfork post-ironic DIY vape. Meh direct trade messenger bag aesthetic echo park copper mug skateboard.
+                <br /><br />
+                Listicle pork belly prism roof party palo santo wolf XOXO humblebrag seitan post-ironic plaid intelligentsia subway tile microdosing. Chambray mixtape organic DIY. Direct trade intelligentsia mustache dreamcatcher kinfolk, vape put a bird on it yr fashion axe tumblr iPhone church-key. IPhone migas kombucha deep v. XOXO marfa echo park scenester vinyl salvia. Skateboard hella woke sriracha, fam jianbing pinterest schlitz seitan austin disrupt blog freegan brunch. Lo-fi DIY la croix viral twee dreamcatcher green juice pug kinfolk post-ironic paleo glossier man braid.
+                <br /><br />
+                Microdosing vexillologist gastropub cray hella, artisan polaroid lo-fi. Direct trade kombucha semiotics, thundercats man braid viral jean shorts dreamcatcher single-origin coffee art party meh shoreditch stumptown try-hard mlkshk. Chambray pinterest pour-over franzen, normcore tumblr waistcoat portland taiyaki asymmetrical. Thundercats tacos letterpress vice truffaut raclette waistcoat fam intelligentsia chia retro.
+                <br /><br />
+                Yr whatever authentic enamel pin, affogato wayfarers normcore bicycle rights next level etsy keytar. Meggings pug whatever offal tilde. Knausgaard vaporware freegan, brunch shoreditch typewriter viral flexitarian microdosing sartorial. Chambray occupy listicle, tbh polaroid organic ennui bicycle rights drinking vinegar iPhone squid. Butcher la croix taxidermy craft beer. Tilde tousled try-hard wayfarers swag ennui four loko lo-fi freegan af edison bulb ramps trust fund vaporware biodiesel. Gentrify hammock bespoke sriracha keytar YOLO kogi hell of skateboard kale chips mixtape PBR&B leggings vinyl gastropub.
+                <br /><br />
+                Peace and Love!
+            </p>
+        </div>
+    );
+}
