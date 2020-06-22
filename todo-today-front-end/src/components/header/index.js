4315437e91af48583c96207b62691b3397eeeae9
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-22 17:03:56 -0500

    populated header component

---
+++ todo-today-front-end/src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>To-Do Today</h1>
+            <h3>Get it To-DONE!</h3>
+        </header>
+    )
+}
