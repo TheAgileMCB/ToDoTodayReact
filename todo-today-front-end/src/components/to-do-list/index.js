3f3903f3405d1f6886be6316c1882a49806f0bef
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-23 12:09:09 -0500

    trying to pull fetch hook into to-do list

--- todo-today-front-end/src/components/to-do-list/index.js
+++ todo-today-front-end/src/components/to-do-list/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './list.scss';
+import useFetch from './../hooks/useFetch';
 
 export default function ToDoList(props) {
     // var str = '8 rabbits, that\'s 16 rabbit ears';
@@ -8,12 +9,13 @@ export default function ToDoList(props) {
     return (
         <div className="list">
             <h1>the LIST</h1>
-            {/* {history.map(item => {
+            <ul>
+                {/* {history.map(item => {
                 <li><h2>{item.title}</h2>
                 <h3><span>{item.startTime}</span><span>{this.props.DueTime}</span></h3>
                 <h3>{item.assignee}</h3></li>
             })} */}
-
+            </ul>
         </div>
     );
 }
