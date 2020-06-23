4019a0adb9fae87f8e717575ebe645eb89b8a1c1
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-22 22:42:08 -0500

    working on connecting form through hooks

--- todo-today-front-end/src/components/to-do-form/index.js
+++ todo-today-front-end/src/components/to-do-form/index.js
@@ -1,40 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Form() {
+export default function Form(props) {
+
+
+    const [taskId, setTaskId] = useState(0);
+    const [title, setTitle] = useState("");
+    const [createdBy, setCreatedBy] = useState("");
+    const [startTime, setStartTime] = useState(Date.now());
+    const [dueTime, setDueTime] = useState(Date.now());
+    const [assignee, setAssignee] = useState("");
+    const [description, setDescription] = useState("");
+    const [estimateTimeToComplete, setEstimatedTimeToComplete] = useState("");
+    const [difficultyRating, setDifficultyRating] = useState(1);
+    const [taskComplete, setTaskComplete] = useState(false);
+
+    function getDataFromApi() {
+        this.props.onReceiveResults(taskId, title, createdBy, startTime, dueTime, assignee, description, estimateTimeToComplete, difficultyRating, taskComplete);
+    }
+
+    // function handleSubmit(e) {
+    //     e.preventDefault();
+    //     let form = e.target;
+
+    //     if (this.state.Title && this.state.CreatedBy) {
+
+    //         this.props.toggleLoading();
+
+    //         //   let request = {
+    //         //     url: this.state.url,
+    //         //     method: this.state.method,
+    //         //   };
+
+    //         getDataFromApi();
+
+    //         // Clear old settings
+    //         //   let url = '';
+    //         //   let method = '';
+
+    //         //   this.setState({ request, url, method });
+    //         form.reset();
+
+    //         this.props.toggleLoading();
+    //     }
+
+    //     else {
+    //         alert('missing information');
+    //     }
+    // }
+
     return (
-      <div className="form-wrapper">
-         <form data-testid="form" className="form" onSubmit={this.handleSubmit}>
-              <label >
-                <span>Task:</span>
-                <input name="Title" type="text" onChange={this.handleChangeTitle} />
-              </label>
-              <label >
-                <span>Complete By:</span>
-                <input name="DueTime" type="time" onChange={this.handleChangeDueTime} />
-              </label>      
-              <label >
-                <span>Assign Someone:</span>
-                <input name="Assignee" type="text" onChange={this.handleChangeAssignee} />
-              </label>      
-              <label >
-                <span>Describe it:</span>
-                <input name="Description" type="text" onChange={this.handleChangeDescription} />
-              </label>
-              <label >
-                <span>Tell them how long it will take:</span>
-                <input name="EstimatedTimeToComplete" type="text" onChange={this.handleChangeEstimatedTimeToComplete} />
-              </label>
-              <label >
-                <span>Tell them how difficult it might be:</span>
-                <input name="DifficultyTating" type="number" onChange={this.handleChangeDifficultyRating} />
-              </label>
-              <label >
-                <input name="TaskId" type="hidden" />
-                <input name="CreatedBy" type="hidden" />
-                <input name="StartTime" type="hidden" />
-              </label>
-              <button type="submit">Get it To-DONE!&trade;</button>
+        <div className="form-wrapper">
+            <form data-testid="form" className="form" onSubmit={getDataFromApi()}>
+                <label >
+                    <span>Task:</span>
+                    <input name="Title" type="text" onChange={setTitle} />
+                </label>
+                <label >
+                    <span>Start After:</span>
+                    <input name="StartTime" type="time" onChange={setStartTime} />
+                </label>
+                <label >
+                    <span>Complete By:</span>
+                    <input name="DueTime" type="time" onChange={setDueTime} />
+                </label>
+                <label >
+                    <span>Assign Someone:</span>
+                    <input name="Assignee" type="text" onChange={setAssignee} />
+                </label>
+                <label >
+                    <span>Describe it:</span>
+                    <input name="Description" type="text" onChange={setDescription} />
+                </label>
+                <label >
+                    <span>Tell them how long it will take:</span>
+                    <input name="EstimatedTimeToComplete" type="time" onChange={setEstimatedTimeToComplete} />
+                </label>
+                <label >
+                    <span>Tell them how difficult it might be:</span>
+                    <input name="DifficultyTating" type="number" onChange={setDifficultyRating} />
+                </label>
+                <label >
+                    <input name="TaskId" type="hidden" value="1" onChange={setTaskId} />
+                    <input name="CreatedBy" type="hidden" value="userId props?" onChange={setCreatedBy} />
+                    <input name="taskComplete" type="hidden" value="false" onChange={setTaskComplete} />
+                </label>
+                <button type="submit">Get it To-DONE!&trade;</button>
             </form>
-      </div>
+        </div>
     );
 }
