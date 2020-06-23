93b7c265fccf46abe2d7140a8c0114103e654ad2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-22 19:27:35 -0500

    added more App routing

--- todo-today-front-end/src/App.js
+++ todo-today-front-end/src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './app.scss';
+
+import Header from './components/header';
+import ToDoList from './components/to-do-list';
+import ToDoForm from './components/to-do-form';
+import ToDoBoard from './components/to-do-board';
+import About from './components/about';
+import License from './components/license';
+import Footer from './components/footer';
 
 function App() {
   return (
@@ -8,6 +17,17 @@ function App() {
     <main>
     <ToDoList />
     <ToDoForm />
+    <Switch>
+      <Route path="/board">
+      <ToDoBoard />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/license">
+        <License />
+      </Route>
+    </Switch>
     </main>
     <Footer />
     </>
