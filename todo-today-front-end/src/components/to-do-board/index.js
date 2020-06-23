b3d1971ad845bbad76db4678d9d93dfa5e67ca5c
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-22 19:49:53 -0500

    built to-do board render component

---
+++ todo-today-front-end/src/components/to-do-board/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function ToDoBoard(props) {
+    // var str = '8 rabbits, that\'s 16 rabbit ears';
+    // str = str.replace(/(\d+)/g,function(a){return Array(+a+1).join('*')});
+
+    return (
+        <div className="card">
+            <h1>{this.props.Title}</h1>
+            <p>{this.props.CreatedBy}</p>
+            <h2><span>{this.props.StartTime}</span><span>{this.props.DueTime}</span></h2>
+            <h2>{this.props.Assignee}</h2>
+            <p>{this.props.Description}</p>
+            <p>{this.props.EstimatedTimeToComplete}</p>
+            {/* <p>int to star transform goes here. consider regex?</p> */}
+        </div>
+    );
+}
