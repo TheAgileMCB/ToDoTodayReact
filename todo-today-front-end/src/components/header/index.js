32225d863e4c546d1659416b2ac03aefdd6c9614
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-23 10:12:11 -0500

    header styles

--- todo-today-front-end/src/components/header/index.js
+++ todo-today-front-end/src/components/header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './header.scss';
 
 export default function Header() {
@@ -6,6 +7,10 @@ export default function Header() {
         <header>
             <h1>To-Do Today</h1>
             <h3>Get it To-DONE!&trade;</h3>
+            <nav>
+                <button className="board-button"><NavLink to="/board">the BOARD</NavLink></button>
+                <button className="home-button"><NavLink exact to="/">the HOME</NavLink></button>
+            </nav>
         </header>
     )
 }
