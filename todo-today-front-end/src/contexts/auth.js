61c38da43cc8fcdea4d4d1e3560565b4fb88423e
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-26 08:41:30 -0500

    refactored auth.js

--- todo-today-front-end/src/contexts/auth.js
+++ todo-today-front-end/src/contexts/auth.js
@@ -1,4 +1,8 @@
 import React, { useContext } from 'react';
+import jwt from 'jsonwebtoken';
+import cookie from 'react-cookies';
+
+const usersAPI = 'https://deltav-todo-alpha.azurewebsites.net/api/v1/Users';
 
 export const AuthContext = React.createContext();
 
@@ -12,13 +16,15 @@ export class AuthProvider extends React.Component {
 
         this.state = {
             user: null,
+            permissions: [],
 
             login: this.login,
+            logout: this.logout,
         };
     }
 
     login = async (username, password) => {
-        const result = await fetch("https://deltav-todo.azurewebsites.net/api/v1/todos/Login", {
+        const result = await fetch(`${usersAPI}/Login`, {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json',
@@ -30,30 +36,64 @@ export class AuthProvider extends React.Component {
         console.log(body);
 
         if (result.ok) {
-            this.setState({ user: body });
-            return;
+            if(this.processToken(body.token, body)){
+                return;
+            }
         }
 
+        //TODO: userError state
         this.logout();
     }
 
     logout = () => {
-        this.setState({ user: null });
+        this.setState({ user: null, permissions: [] });
+        cookie.remove('auth');
+    }
+
+    processToken(token, user) {
+        try {
+            const payload = jwt.decode(token);
+            if (payload) {
+                if (!user) {
+                    user = {
+                        id: payload['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'],
+            username: payload['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
+                    };
+                }
+
+                this.setState({
+                    user,
+                    permissions: payload.permissions || [],
+                });
+                cookie.save('auth', token);
+                return true;
+            }
+        } catch(e) {
+            console.warn(e);
+            this.logout();
+        }
+    }
+
+    componentDidMount() {
+        const cookieToken = cookie.load('auth');
+        if (cookieToken) console.log('Found auth cookie!');
+
+        this.processToken(cookieToken);
     }
 
     render() {
 
-        const { user } = this.context;
+        // const { user } = this.context;
 
-        if (user) {
-            return (
-                <>
-                    <h3>Welcome back, {user.username}!</h3>
-                    <form onSubmit={this.logoutSubmit}>
-                        <button>Log Out</button>
-                    </form>
-                </>)
-        }
+        // if (user) {
+        //     return (
+        //         <>
+        //             <h3>Welcome back, {user.username}!</h3>
+        //             <form onSubmit={this.logoutSubmit}>
+        //                 <button>Log Out</button>
+        //             </form>
+        //         </>)
+        // }
 
         return (
             <AuthContext.Provider value={this.state}>
